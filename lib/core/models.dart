916b18540acaccc95f0846c1d309eb9c6f9af8e2
Utsob Roy <uroybd@gmail.com>
2019-01-02 10:09:58 +0600

    wip:some works [master]

--- lib/core/models.dart
+++ lib/core/models.dart
@@ -53,6 +53,9 @@ class AccountDetail {
 }
 
 class MovieList {
+  // MovieList Result Object
+  //
+  // https://developers.themoviedb.org/3/account/get-created-lists
   String description;
   int favoriteCount;
   int id;
@@ -93,6 +96,9 @@ abstract class Page {
 }
 
 class MovieListResponse extends Page {
+  // MovieList Response Data
+  //
+  // https://developers.themoviedb.org/3/account/get-created-lists
   List<MovieList> results;
 
   MovieListResponse({int page, this.results, int totalPages, int totalResults})
@@ -259,6 +265,238 @@ class Logo extends Image {
       : super(data['aspect_ratio'], data['file_path'], data['height'],
             data['vote_average'], data['vote_count'], data['width']) {
     this.fileType = data['file_type'];
+  }
+}
+
+class Person {
+  // Person
+  //
+  // https://developers.themoviedb.org/3/people/get-person-details
+
+  int id;
+  String name;
+  List<String> alsoKnownAs;
+  String birthday;
+  String knownForDepartment;
+  String deathday;
+  int gender;
+  String biography;
+  num popularity;
+  String placeOfBirth;
+  String profilePath;
+  bool adult;
+  String imdbID;
+  String homepage;
+
+  Person(
+      {this.id,
+      this.name,
+      this.alsoKnownAs,
+      this.birthday,
+      this.knownForDepartment,
+      this.deathday,
+      this.gender,
+      this.biography,
+      this.popularity,
+      this.placeOfBirth,
+      this.profilePath,
+      this.adult,
+      this.imdbID,
+      this.homepage});
+
+  Person.fromJSON(Map<String, dynamic> data) {
+    this.id = data['id'];
+    this.name = data['name'];
+    this.birthday = data['birthday'];
+    this.knownForDepartment = data['known_for_department'];
+    this.deathday = data['deathday'];
+    this.gender = data['gender'];
+    this.biography = data['biography'];
+    this.popularity = data['popularity'];
+    this.placeOfBirth = data['place_of_birth'];
+    this.profilePath = data['profile_path'];
+    this.adult = data['adult'];
+    this.imdbID = data['imdb_id'];
+    this.homepage = data['homepage'];
+  }
+}
+
+class TVEpisode {
+  // TV Show Episode
+  //
+  // https://developers.themoviedb.org/3/tv-episodes/get-tv-episode-details
+  int id;
+  String airDate;
+  List<Person> crew;
+  List<Person> guestStars;
+  String name;
+  String overview;
+  String productionCode;
+  int seasonNumber;
+  String stillPath;
+  num voteAverage;
+  int voteCount;
+
+  TVEpisode(
+      {this.id,
+      this.airDate,
+      this.crew,
+      this.guestStars,
+      this.name,
+      this.overview,
+      this.productionCode,
+      this.seasonNumber,
+      this.stillPath,
+      this.voteAverage,
+      this.voteCount});
+
+  TVEpisode.fromJSON(Map<String, dynamic> data) {
+    this.id = data['id'];
+    this.airDate = data['air_date'];
+    this.crew = data['crew'].map((d) => new Person.fromJSON(d));
+    this.guestStars = data['guest_stars'].map((d) => new Person.fromJSON(d));
+    this.name = data['name'];
+    this.overview = data['overview'];
+    this.productionCode = data['production_code'];
+    this.seasonNumber = data['season_number'];
+    this.stillPath = data['still_path'];
+    this.voteAverage = data['vote_average'];
+    this.voteCount = data['vote_count'];
+  }
+}
+
+class TVSeason {
+  // TV Show Season
+  //
+  // https://developers.themoviedb.org/3/tv-seasons/get-tv-season-details
+
+  String hashID;
+  String airDate;
+  List<TVEpisode> episodes;
+  String name;
+  String overview;
+  int id;
+  String posterPath;
+  int seasonNumber;
+  int episodeCount;
+
+  TVSeason(
+      {this.hashID,
+      this.airDate,
+      this.episodes,
+      this.name,
+      this.overview,
+      this.id,
+      this.posterPath,
+      this.seasonNumber});
+
+  TVSeason.fromJSON(Map<String, dynamic> data) {
+    this.hashID = data["_id"];
+    this.airDate = data['air_date'];
+    this.episodes = data['episodes'].map((d) => new TVEpisode.fromJSON(d));
+    this.name = data['name'];
+    this.overview = data['overview'];
+    this.posterPath = data['poster_path'];
+    this.seasonNumber = data['season_number'];
+    this.episodeCount =
+        (data['episode_count']) ? data['episode_count'] : this.episodes.length;
+  }
+}
+
+class TV {
+  // TV Show
+  //
+  // https://developers.themoviedb.org/3/tv/get-tv-details
+
+  int id;
+  String backdropPath;
+  Person createdBy;
+  List<int> episodeRunTime;
+  String firstAirDate;
+  List<Genre> genres;
+  String homepage;
+  bool inProduction;
+  List<String> languages;
+  String lastAirDate;
+  TVEpisode lastEpisodeToAir;
+  String name;
+  TVEpisode nextEpisodeToAir;
+  List<Network> networks;
+  int numberOfEpisodes;
+  int numberOfSeasons;
+  List<String> originCountry;
+  String originalLanguage;
+  String originalName;
+  String overview;
+  num popularity;
+  String posterPath;
+  List<ProductionCompany> productionCompanies;
+  List<TVSeason> seasons;
+  String status;
+  String type;
+  num voteAverage;
+  int voteCount;
+
+  TV(
+      {this.id,
+      this.backdropPath,
+      this.createdBy,
+      this.episodeRunTime,
+      this.firstAirDate,
+      this.genres,
+      this.homepage,
+      this.inProduction,
+      this.languages,
+      this.lastAirDate,
+      this.lastEpisodeToAir,
+      this.name,
+      this.nextEpisodeToAir,
+      this.networks,
+      this.numberOfEpisodes,
+      this.numberOfSeasons,
+      this.originCountry,
+      this.originalLanguage,
+      this.originalName,
+      this.overview,
+      this.popularity,
+      this.posterPath,
+      this.productionCompanies,
+      this.seasons,
+      this.status,
+      this.type,
+      this.voteAverage,
+      this.voteCount});
+
+  TV.toJSON(Map<String, dynamic> data) {
+    this.id = data['id'];
+    this.backdropPath = data['backdrop_path'];
+    this.createdBy = data['created_by'];
+    this.episodeRunTime = data['episode_run_time'];
+    this.firstAirDate = data['first_air_date'];
+    this.genres = data['genres'];
+    this.homepage = data['homepage'];
+    this.inProduction = data['in_production'];
+    this.languages = data['languages'];
+    this.lastAirDate = data['last_air_date'];
+    this.lastEpisodeToAir = data['last_episode_to_air'];
+    this.name = data['name'];
+    this.nextEpisodeToAir = data['next_episode_to_air'];
+    this.networks = data['networks'].map((d) => new Network.fromJSON(d));
+    this.numberOfEpisodes = data['number_of_episodes'];
+    this.numberOfSeasons = data['number_of_seasons'];
+    this.originCountry = data['origin_country'];
+    this.originalLanguage = data['original_language'];
+    this.originalName = data['original_originalName'];
+    this.overview = data['overview'];
+    this.popularity = data['popularity'];
+    this.posterPath = data['poster_path'];
+    this.productionCompanies = data['production_companies']
+        .map((d) => new ProductionCompany.fromJSON(d));
+    this.seasons = data['seasons'].map((d) => new TVSeason(d));
+    this.status = data['status'];
+    this.type = data['type'];
+    this.voteAverage = data['vote_average'];
+    this.voteCount = data['vote_count'];
   }
 }
 
